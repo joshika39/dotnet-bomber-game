6680a4bd956951ab7b8a125b9a8146cd4432f56d
Joshua Hegedus <josh.hegedus@outlook.com>
2023-10-19 22:49:10 +0200

    added Ui int tests

---
+++ Bomber.UI.Forms.Int.Tests/MapGeneratorTests.cs
@@ -0,0 +1,20 @@
+﻿using Bomber.UI.Forms.MapGenerator;
+using Xunit;
+
+namespace Bomber.UI.Forms.Int.Tests
+{
+    public class MapGeneratorTests
+    {
+        [Fact]
+        public void MGT_0001_Given_NullParameter_When_ConstructorCalled_Then_ThrowsException()
+        {
+            var exception = Record.Exception(() =>
+            {
+                _ = new MapGeneratorWindowPresenter(null!);
+            });
+
+            Assert.NotNull(exception);
+            Assert.IsType<ArgumentNullException>(exception);
+        }
+    }
+}
