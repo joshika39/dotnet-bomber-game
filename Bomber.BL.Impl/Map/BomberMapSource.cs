e0a325f4bad649ecdc1441b5e3195e4ea7b9f7a5
Joshua Hegedus <47304430+joshika39@users.noreply.github.com>
2023-12-13 01:19:01 +0100

    Generator fix (#19)
    * added fixes
    
    * fixed readme

--- Bomber.BL.Impl/Map/BomberMapSource.cs
+++ Bomber.BL.Impl/Map/BomberMapSource.cs
@@ -1,4 +1,5 @@
-﻿using Bomber.BL.Entities;
+﻿using System.Text;
+using Bomber.BL.Entities;
 using Bomber.BL.Map;
 using Bomber.BL.MapGenerator.DomainModels;
 using GameFramework.Core.Factories;
@@ -6,13 +7,17 @@ using GameFramework.Core.Position;
 using GameFramework.Entities;
 using GameFramework.Impl.Map.Source;
 using GameFramework.Map.MapObject;
+using Infrastructure.IO;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Bomber.BL.Impl.Map
 {
-    public class BomberMapSource : JsonMapSource2D, IBomberMapSource
+    public class BomberMapSource : JsonMapSource2D<TileType>, IBomberMapSource
     {
-        private readonly IPositionFactory _factory;
+        private readonly IReader _testReader;
+        private readonly IPositionFactory _fact;
+        private readonly IMapObject2DConverter _converter;
+        private readonly string _base64;
         public string Name { get; set; }
         public string Description { get; set; }
         public IEnumerable<DummyEntity> Enemies { get; set; }
@@ -20,14 +25,19 @@ namespace Bomber.BL.Impl.Map
         public IPosition2D PlayerPosition { get; set; }
 
 
-        public BomberMapSource(IServiceProvider provider, string filePath, int[,] data, ICollection<IUnit2D> units, int col, int row, string name, string description) : base(provider, filePath, data, units, col, row)
+        public BomberMapSource(IServiceProvider provider, string filePath, int[,] data, ICollection<IUnit2D> units, ICollection<DummyEntity> dummyEntities, int col, int row, string name, string description) : base(provider, filePath, data, units, col, row)
         {
             Name = name ?? throw new ArgumentNullException(nameof(name));
+            _testReader = provider.GetRequiredService<IReader>();
+            _fact = provider.GetRequiredService<IPositionFactory>();
+            _converter = provider.GetRequiredService<IMapObject2DConverter>();
             Description = description ?? throw new ArgumentNullException(nameof(description));
-            Enemies = units.Select(unit => new DummyEntity(unit.Position.X, unit.Position.Y));
+            Enemies = dummyEntities;
             Bombs = new List<DummyBomb>();
-            _factory = provider.GetRequiredService<IPositionFactory>();
-            PlayerPosition = _factory.CreatePosition(1, 1);
+            var factory = provider.GetRequiredService<IPositionFactory>();
+            PlayerPosition = factory.CreatePosition(1, 1);
+            _base64 = Convert.ToBase64String(Encoding.UTF8.GetBytes(GetRawData(data)));
+            SaveInitState();
         }
 
         public BomberMapSource(IServiceProvider provider, string filePath) : base(provider, filePath)
@@ -38,8 +48,8 @@ namespace Bomber.BL.Impl.Map
             Bombs = Query.GetObject<List<DummyBomb>>("bombs") ?? new List<DummyBomb>();
             var x = Query.GetIntAttribute("player.x") ?? 1;
             var y = Query.GetIntAttribute("player.y") ?? 1;
-            _factory = provider.GetRequiredService<IPositionFactory>();
-            PlayerPosition = _factory.CreatePosition(x, y);
+            var factory = provider.GetRequiredService<IPositionFactory>();
+            PlayerPosition = factory.CreatePosition(x, y);
         }
 
         public override void SaveLayout(IEnumerable<IMapObject2D> updatedMapObjects, IEnumerable<IUnit2D> updatedUnits)
@@ -56,6 +66,61 @@ namespace Bomber.BL.Impl.Map
             Bombs = units.Where(u => u is IBomb).Select(unit => new DummyBomb(unit.Position.X, unit.Position.Y, (unit as IBomb)!.Radius, (unit as IBomb)!.RemainingTime));
             Query.SetObject("enemies", Enemies);
             Query.SetObject("bombs", Bombs);
+        }
+
+        private void SaveInitState()
+        {
+            Query.SetAttribute("player.x", PlayerPosition.X);
+            Query.SetAttribute("player.y", PlayerPosition.Y);
+            Query.SetAttribute("description", Description);
+            Query.SetAttribute("name", Name);
+            Query.SetAttribute("row", RowCount);
+            Query.SetAttribute("col", ColumnCount);
+            Query.SetAttribute("data", _base64);
+            Query.SetObject("enemies", Enemies);
+            Query.SetObject("bombs", Bombs);
+        }
+        
+        protected virtual IEnumerable<IMapObject2D> ConvertDataToObjects()
+        {
+            var id = Guid.NewGuid();
+            var tempPath = Path.Join(Path.GetTempPath(), $"{id}.txt");
+            File.Create(tempPath).Close();
+            File.WriteAllText(tempPath, Encoding.UTF8.GetString(Convert.FromBase64String(_base64)));
+            using var stream = new StreamReader(tempPath);
+            var mapLayout = _testReader.ReadAllLines<int>(stream, int.TryParse, ' ').ToList();
+            var list = new List<IMapObject2D>();
+            for (var i = 0; i < mapLayout.Count; i++)
+            {
+                var row = mapLayout[i].ToList();
+                for (var j = 0; j < row.Count; j++)
+                {
+                    var value = row[j];
+                    var position = _fact.CreatePosition(j, i);
+                    if (!Enum.TryParse(value.ToString(), out TileType type))
+                    {
+                        continue;
+                    }
+                    list.Add(_converter.FromEnum(type, position));
+                }
+            }
+            return list;
+        }
+        
+        private string GetRawData(int[,] data)
+        {
+            var stringBuilder = new StringBuilder();
+            for (var i = 0; i < RowCount; i++)
+            {
+                for (var j = 0; j < ColumnCount; j++)
+                {
+                    var tile = data[i,j];
+                    stringBuilder.Append($"{tile} ");
+                }
+                stringBuilder.Remove(stringBuilder.Length - 1, 1);
+                stringBuilder.Append("\r\n");
+            }
+            return stringBuilder.ToString();
         }
     }
 }
--- README.md
+++ README.md
@@ -1,10 +1,9 @@
 # .Net Bomber man
+
+## ⚠️ Deprecated ⚠️
+
+The game had been refactored and move to [[joshika39/dotnet-bomber-v2](https://github.com/joshika39/dotnet-bomber-v2)]
+
 ## Overview
 A C# implementation of the famous BomberMan
 
-### Stats
-[![Bugs](https://analysis.bitof.faith/api/project_badges/measure?project=joshika39_dotnet-bomber-game_AYtEcIxBdB6aqkaimYlg&metric=bugs&token=sqb_434c66dd1dd367a57484481c07cb65c5e6480d2a)](https://analysis.bitof.faith/dashboard?id=joshika39_dotnet-bomber-game_AYtEcIxBdB6aqkaimYlg)  [![Coverage](https://analysis.bitof.faith/api/project_badges/measure?project=joshika39_dotnet-bomber-game_AYtEcIxBdB6aqkaimYlg&metric=coverage&token=sqb_434c66dd1dd367a57484481c07cb65c5e6480d2a)](https://analysis.bitof.faith/dashboard?id=joshika39_dotnet-bomber-game_AYtEcIxBdB6aqkaimYlg)
-[![Reliability Rating](https://analysis.bitof.faith/api/project_badges/measure?project=joshika39_dotnet-bomber-game_AYtEcIxBdB6aqkaimYlg&metric=reliability_rating&token=sqb_434c66dd1dd367a57484481c07cb65c5e6480d2a)](https://analysis.bitof.faith/dashboard?id=joshika39_dotnet-bomber-game_AYtEcIxBdB6aqkaimYlg)
-
-### Quality gate
-[![Quality gate](https://analysis.bitof.faith/api/project_badges/quality_gate?project=joshika39_dotnet-bomber-game_AYtEcIxBdB6aqkaimYlg&token=sqb_434c66dd1dd367a57484481c07cb65c5e6480d2a)](https://analysis.bitof.faith/dashboard?id=joshika39_dotnet-bomber-game_AYtEcIxBdB6aqkaimYlg)
